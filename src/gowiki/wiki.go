ea16e753b340b6a733b332a52e23e36632a18b1d
Matt Sloan <matt.sloan@concordusa.com>
2018-07-02 20:26:18 -0500

    initial commit

---
+++ src/gowiki/wiki.go
@@ -0,0 +1,43 @@
+package main
+
+// package fmt implements formatted I/O with functions analagous to C's functions printf and scanf
+// package io provides basic interfaces to I/O primitives
+// package ioutil implements I/O utilitity functions
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"log"
+)
+
+type Page struct {
+	Title string
+	Body []byte
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
+func main() {
+	http.HandleFunc("/view/", viewHandler)
+	log.Fatal(http.ListenAndServe(":8080",nil))
+}
+
+
